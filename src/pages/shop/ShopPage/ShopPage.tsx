c2399e28663249616c0fc037bf4c041067d70b1d
ivans204 <ivansvrga204@gmail.com>
2021-08-27 16:07:16 +0200

    ShopPage item loop

--- src/pages/shop/ShopPage/ShopPage.tsx
+++ src/pages/shop/ShopPage/ShopPage.tsx
@@ -1,3 +1,30 @@
+import { ShopItem } from 'components/ShopItem';
+
+import { IProduct, products } from '../products';
+
 export const ShopPage = () => {
-    return <div>Shop Page</div>;
+    return (
+        <>
+            {products.map(
+                ({
+                    id,
+                    title,
+                    price,
+                    description,
+                    category,
+                    image,
+                }: IProduct) => (
+                    <ShopItem
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        description={description}
+                        category={category}
+                        image={image}
+                    />
+                )
+            )}
+        </>
+    );
 };
