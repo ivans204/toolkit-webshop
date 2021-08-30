8f4da095e6c52eef196760553faa904fc6137ab1
ivans204 <ivansvrga204@gmail.com>
2021-08-30 15:32:03 +0200

    ShopItem layout

--- src/components/ShopItem/ShopItem.tsx
+++ src/components/ShopItem/ShopItem.tsx
@@ -1,21 +1,93 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { IProduct } from 'pages/shop/products';
+import {
+    Avatar,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    Collapse,
+    IconButton,
+    Typography,
+} from '@material-ui/core';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        media: {
+            height: 250,
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto !important',
+            transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+            }),
+        },
+        expandOpen: {
+            transform: 'rotate(180deg)',
+        },
+        header: {
+            minHeight: '116px',
+        },
+    })
+);
 
 export const ShopItem: FC<IProduct> = ({
-    id,
     title,
     price,
     description,
     category,
     image,
 }) => {
+    const classes = useStyles();
+
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
     return (
-        <div>
-            {title}
-            {price}
-            {description}
-            {category}
-            {image}
-        </div>
+        <Card>
+            <CardHeader
+                className={classes.header}
+                avatar={<Avatar aria-label="recipe">{title[0]}</Avatar>}
+                title={
+                    <Typography variant="body2" aria-label="recipe">
+                        {title}
+                    </Typography>
+                }
+                subheader={category}
+            />
+            <CardMedia
+                image={image}
+                title={title}
+                className={classes.media}
+                style={{ backgroundSize: 'contain' }}
+            />
+
+            <CardActions>
+                <Button variant="outlined">Add To Cart</Button>
+                <IconButton
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                    className={`${classes.expand} ${
+                        expanded && classes.expandOpen
+                    }`}
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>{description}</CardContent>
+            </Collapse>
+        </Card>
     );
 };
