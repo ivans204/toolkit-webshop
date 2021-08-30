39fe3737a86d9c6d5db8aa2a6e125bda1fb08616
ivans204 <ivansvrga204@gmail.com>
2021-08-30 15:32:15 +0200

    ShopPage layout

--- src/pages/shop/ShopPage/ShopPage.tsx
+++ src/pages/shop/ShopPage/ShopPage.tsx
@@ -1,30 +1,50 @@
 import { ShopItem } from 'components/ShopItem';
 
-import { IProduct, products } from '../products';
+import { IProduct } from '../products';
+
+import { useGetProductsQuery } from 'app/services/products';
+
+import { Container, Grid } from '@material-ui/core';
 
 export const ShopPage = () => {
+    const { data, error, isLoading } = useGetProductsQuery(
+        'https://fakestoreapi.com/products'
+    );
+
+    if (isLoading) return <h1>Loading...</h1>;
+    if (error) return <h1>Error...</h1>;
+
     return (
-        <>
-            {products.map(
-                ({
-                    id,
-                    title,
-                    price,
-                    description,
-                    category,
-                    image,
-                }: IProduct) => (
-                    <ShopItem
-                        key={id}
-                        id={id}
-                        title={title}
-                        price={price}
-                        description={description}
-                        category={category}
-                        image={image}
-                    />
-                )
-            )}
-        </>
+        <Container style={{ marginTop: '50px' }}>
+            <Grid
+                container
+                direction="row"
+                spacing={3}
+                alignContent="space-between"
+                justifyContent="center"
+            >
+                {data?.map(
+                    ({
+                        id,
+                        title,
+                        price,
+                        description,
+                        category,
+                        image,
+                    }: IProduct) => (
+                        <Grid item key={id} xs={4}>
+                            <ShopItem
+                                id={id}
+                                title={title}
+                                price={price}
+                                description={description}
+                                category={category}
+                                image={image}
+                            />
+                        </Grid>
+                    )
+                )}
+            </Grid>
+        </Container>
     );
 };
