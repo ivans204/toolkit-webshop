81f82bfe3d6490e1724210ac07405ea9965e261e
ivans204 <ivansvrga204@gmail.com>
2021-09-01 15:40:38 +0200

    remove Paper component

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -7,7 +7,6 @@ import {
     CardContent,
     CardMedia,
     Grid,
-    Paper,
     Typography,
 } from '@material-ui/core';
 
@@ -17,9 +16,6 @@ import Mens from '../../assets/images/mens.jpg';
 import Womens from '../../assets/images/womens.jpg';
 
 const useStyles = makeStyles({
-    root: {
-        marginTop: '25px',
-    },
     imgMaxHeight: {
         maxHeight: '500px',
     },
@@ -45,73 +41,69 @@ const useStyles = makeStyles({
 export const Categories = () => {
     const classes = useStyles();
     return (
-        <Paper elevation={0} className={classes.root}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Card>
-                        <CardActionArea component={Link} to="/electronics">
-                            <CardContent className={classes.cardContentTitle}>
-                                <Typography variant="h4">
-                                    Electronics
-                                </Typography>
-                            </CardContent>
-                            <CardMedia
-                                component="img"
-                                image={Electronics}
-                                alt="Electronics category"
-                                title="Electronics category"
-                                className={classes.imgMaxHeight}
-                            />
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card>
-                        <CardActionArea component={Link} to="/men's clothing">
-                            <CardContent className={classes.cardContentTitle}>
-                                <Typography variant="h4">Mens</Typography>
-                            </CardContent>
-                            <CardMedia
-                                component="img"
-                                image={Mens}
-                                alt="Mens category"
-                                title="Mens category"
-                            />
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card>
-                        <CardActionArea component={Link} to="/jewelery">
-                            <CardContent className={classes.cardContentTitle}>
-                                <Typography variant="h4">Jewelery</Typography>
-                            </CardContent>
-                            <CardMedia
-                                component="img"
-                                image={Jewelery}
-                                alt="Jewelery category"
-                                title="Jewelery category"
-                            />
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={12} md={4}>
-                    <Card>
-                        <CardActionArea component={Link} to="/women's clothing">
-                            <CardContent className={classes.cardContentTitle}>
-                                <Typography variant="h4">Womens</Typography>
-                            </CardContent>
-                            <CardMedia
-                                component="img"
-                                image={Womens}
-                                alt="Womens category"
-                                title="Womens category"
-                                className={classes.imgMaxHeight}
-                            />
-                        </CardActionArea>
-                    </Card>
-                </Grid>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Card>
+                    <CardActionArea component={Link} to="/electronics">
+                        <CardContent className={classes.cardContentTitle}>
+                            <Typography variant="h4">Electronics</Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            image={Electronics}
+                            alt="Electronics category"
+                            title="Electronics category"
+                            className={classes.imgMaxHeight}
+                        />
+                    </CardActionArea>
+                </Card>
             </Grid>
-        </Paper>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                    <CardActionArea component={Link} to="/men's clothing">
+                        <CardContent className={classes.cardContentTitle}>
+                            <Typography variant="h4">Mens</Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            image={Mens}
+                            alt="Mens category"
+                            title="Mens category"
+                        />
+                    </CardActionArea>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                    <CardActionArea component={Link} to="/jewelery">
+                        <CardContent className={classes.cardContentTitle}>
+                            <Typography variant="h4">Jewelery</Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            image={Jewelery}
+                            alt="Jewelery category"
+                            title="Jewelery category"
+                        />
+                    </CardActionArea>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+                <Card>
+                    <CardActionArea component={Link} to="/women's clothing">
+                        <CardContent className={classes.cardContentTitle}>
+                            <Typography variant="h4">Womens</Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            image={Womens}
+                            alt="Womens category"
+                            title="Womens category"
+                            className={classes.imgMaxHeight}
+                        />
+                    </CardActionArea>
+                </Card>
+            </Grid>
+        </Grid>
     );
 };
