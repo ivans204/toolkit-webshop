49f009bcfc5238b73ae2df24a2c372a1ef7d6ce3
ivans204 <ivansvrga204@gmail.com>
2021-09-02 14:00:00 +0200

    create cartSlice and add to cart reducer

---
+++ src/features/cart/cartSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+
+import { IProduct } from 'pages/ShopPage/products';
+
+interface CartState {
+    items: IProduct[];
+}
+
+const initialState: CartState = {
+    items: [],
+};
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addToCart: (state, action: PayloadAction<IProduct>) => {
+            state.items.push(action.payload);
+        },
+    },
+});
+
+export const { addToCart } = cartSlice.actions;
+export const selectCart = (state: RootState) => state.cart.items;
+export default cartSlice.reducer;
