d135bc8def0de753309f7a37fb2928cf94d3eeea
ivans204 <ivansvrga204@gmail.com>
2021-09-02 09:46:28 +0200

    change shop grid breakpoints

---
+++ src/pages/ShopPage/ShopPage.tsx
@@ -0,0 +1,57 @@
+import { FC } from 'react';
+import './style.css';
+import { ShopItem } from 'components/ShopItem';
+
+import { IProduct } from './products';
+
+import { useGetCategoryProductsQuery } from 'app/services/products';
+
+import Masonry from 'react-masonry-css';
+
+interface ShopPageProps {
+    categoryName: string;
+}
+
+export const ShopPage: FC<ShopPageProps> = ({ categoryName }) => {
+    const { data, error, isLoading } =
+        useGetCategoryProductsQuery(categoryName);
+
+    const breakpoints = {
+        default: 3,
+        '600': 1,
+        '960': 2,
+    };
+
+    if (isLoading) return <h1>Loading...</h1>;
+    if (error) return <h1>Error...</h1>;
+
+    return (
+        <Masonry
+            breakpointCols={breakpoints}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+        >
+            {data?.map(
+                ({
+                    id,
+                    title,
+                    price,
+                    description,
+                    category,
+                    image,
+                }: IProduct) => (
+                    <div key={id}>
+                        <ShopItem
+                            id={id}
+                            title={title}
+                            price={price}
+                            description={description}
+                            category={category}
+                            image={image}
+                        />
+                    </div>
+                )
+            )}
+        </Masonry>
+    );
+};
