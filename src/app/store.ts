d7befe54bb4c2df84a701eb2a2f00a8c5d4268bb
ivans204 <ivansvrga204@gmail.com>
2021-09-02 13:58:00 +0200

    add cartReducer and redux-logger to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,17 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
+import logger from 'redux-logger';
+
+import cartReducer from '../features/cart/cartSlice';
 import { productApi } from 'app/services/products';
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
+        cart: cartReducer,
         [productApi.reducerPath]: productApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(productApi.middleware),
+        getDefaultMiddleware().concat(productApi.middleware).concat(logger),
 });
 
 export type AppDispatch = typeof store.dispatch;
