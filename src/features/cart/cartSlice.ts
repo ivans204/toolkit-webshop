3c645aad4dfa7bf98aca4a16223a83432b2780f9
ivans204 <ivansvrga204@gmail.com>
2021-09-07 10:45:14 +0200

    add mock initialState, add and remove item action

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -8,7 +8,38 @@ export interface CartState {
 }
 
 const initialState: CartState = {
-    items: [],
+    items: [
+        {
+            id: 15,
+            title: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+            price: 56.99,
+            description:
+                'Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates',
+            category: "women's clothing",
+            image: 'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg',
+            count: 1,
+        },
+        {
+            id: 16,
+            title: "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+            price: 29.95,
+            description:
+                '100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON',
+            category: "women's clothing",
+            image: 'https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg',
+            count: 1,
+        },
+        {
+            id: 17,
+            title: 'Rain Jacket Women Windbreaker Striped Climbing Raincoats',
+            price: 39.99,
+            description:
+                "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
+            category: "women's clothing",
+            image: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg',
+            count: 1,
+        },
+    ],
 };
 
 export const cartSlice = createSlice({
@@ -18,9 +49,25 @@ export const cartSlice = createSlice({
         addToCart: (state, action: PayloadAction<IProduct>) => {
             state.items.push(action.payload);
         },
+        addItem: (state, action: PayloadAction<number>) => {
+            state.items.find((item) => {
+                if (item.id === action.payload) return item.count++;
+                return null;
+            });
+        },
+        removeItem: (state, action: PayloadAction<number>) => {
+            state.items.find((item) => {
+                if (item.id === action.payload) return item.count--;
+                return null;
+            });
+        },
+        removeFromCart: (state, action: PayloadAction<number>) => {
+            state.items.filter((item) => item.id !== action.payload);
+        },
     },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, addItem, removeItem, removeFromCart } =
+    cartSlice.actions;
 export const selectCart = (state: RootState) => state.cart.items;
 export default cartSlice.reducer;
