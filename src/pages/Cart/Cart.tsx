d4ebe32bd1670cbfaae27876b45bcb04b54e5d8c
ivans204 <ivansvrga204@gmail.com>
2021-09-09 13:18:37 +0200

    add total amount to Cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -69,6 +69,11 @@ export const Cart = () => {
     const cartData = useSelector(selectCart);
     const classes = useStyles();
 
+    const priceTotal = cartData.reduce(
+        (prev, current) => +(prev + current.price * current.count).toFixed(2),
+        0
+    );
+
     return (
         <Grid container spacing={3}>
             <Grid item xs={12} sm={9}>
@@ -165,7 +170,36 @@ export const Cart = () => {
             <Grid item xs={12} sm={3}>
                 <Paper>
                     <Card>
-                        <CardContent>TOTAL</CardContent>
+                        <CardHeader title="Total" />
+                        <CardContent>
+                            <p>
+                                Amount:{' '}
+                                <span style={{ float: 'right' }}>
+                                    ${priceTotal}
+                                </span>
+                            </p>
+                            <p>
+                                Shipping:{' '}
+                                <span style={{ float: 'right' }}>Free</span>
+                            </p>
+                            <Divider />
+                            <Typography
+                                variant="h6"
+                                style={{ marginTop: '16px' }}
+                            >
+                                Total after tax:{' '}
+                                <span style={{ float: 'right' }}>
+                                    ${priceTotal}
+                                </span>
+                            </Typography>
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                style={{ width: '100%', marginTop: '16px' }}
+                            >
+                                Go To Checkout
+                            </Button>
+                        </CardContent>
                     </Card>
                 </Paper>
             </Grid>
